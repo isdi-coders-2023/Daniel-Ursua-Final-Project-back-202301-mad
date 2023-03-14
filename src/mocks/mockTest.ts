be60c49ad986426452dc76be40464dfe25bb5933
Daniel <dursuaf@gmail.com>
2023-03-14 15:32:39 +0100

    Add mock test and db connect

---
+++ src/mocks/mockTest.ts
@@ -0,0 +1,38 @@
+import { Response, Request, NextFunction } from 'express';
+
+export const mockReq = {
+  body: {
+    email: 'test email',
+    passwd: 'test passwd',
+    name: 'test name',
+  },
+} as unknown as Request;
+
+export const mockReq1 = {
+  body: {
+    passwd: 'test passwd',
+    name: 'test name',
+  },
+} as unknown as Request;
+export const mockReq2 = {
+  body: {
+    email: 'test email',
+    name: 'test name',
+  },
+} as unknown as Request;
+export const mockReq3 = {
+  body: {
+    email: 'test email',
+    passwd: 'test passwd',
+  },
+} as unknown as Request;
+export const mockReq4 = {
+  body: {},
+} as unknown as Request;
+
+export const mockResp = {
+  status: jest.fn(),
+  json: jest.fn(),
+} as unknown as Response;
+
+export const mockNext = jest.fn();
