40684c54e9b5dba01ae8c7b6995193774120e4c8
Daniel <dursuaf@gmail.com>
2023-03-20 10:56:51 +0100

    Update interfaces and imports

--- src/controllers/plant.controller.ts
+++ src/controllers/plant.controller.ts
@@ -1,65 +1,39 @@
 import { Response, Request, NextFunction } from 'express';
 import createDebug from 'debug';
-import { Plant } from '../entities/plant.js';
 import { HTTPError } from '../errors/error.js';
-import { Repo } from '../repositories/repo.interface.js';
 import jwt from 'jsonwebtoken';
 import { UsersMongoRepo } from '../repositories/users/users.mongo.repo.js';
+import { PlantRepo } from '../repositories/plants/plant.interface.js';
 
 const debug = createDebug('WFP:controller: plants');
 
 export class PlantsController {
-  constructor(public repo: Repo<Plant>) {
+  constructor(public repo: PlantRepo) {
     debug('Instantiate');
   }
-  async checkUser(req: Request, resp: Response, next: NextFunction) {
-    try {
-      debug('Searching');
-      if (!req.headers.authorization)
-        throw new HTTPError(400, 'Bad request', 'Request required');
-      const token = req.headers.authorization.split(' ')[1];
-      const decodedToken = await jwt.verify(
-        token,
-        process.env.JWT_SECRET as string
-      );
-      if (typeof decodedToken !== 'object')
-        throw new HTTPError(401, 'Unauthorized', 'Invalid token');
-      const userMail = decodedToken.email;
-      resp.status(200);
-      return userMail;
-    } catch (error) {
-      next(error);
-    }
-  }
 
-  async add(req: Request, resp: Response, next: NextFunction) {
+  async add(Customreq: Request, resp: Response, next: NextFunction) {
     try {
       debug('add: post');
       if (
-        !req.body.photo ||
-        !req.body.name ||
-        !req.body.ubication ||
-        !req.body.height ||
-        !req.body.lightness ||
-        !req.body.humidity ||
-        !req.body.difficult ||
-        !req.body.animalFriendly
+        !Customreq.body.photo ||
+        !Customreq.body.name ||
+        !Customreq.body.ubication ||
+        !Customreq.body.height ||
+        !Customreq.body.lightness ||
+        !Customreq.body.humidity ||
+        !Customreq.body.difficult ||
+        !Customreq.body.animalFriendly
       )
         throw new HTTPError(400, 'Bad request', 'Incomplete information');
       const data = await this.repo.search({
         key: 'name',
-        value: req.body.name,
+        value: Customreq.body.name,
       });
       if (data.length)
         throw new HTTPError(409, 'Conflict', 'Register already exist');
-      const userMail = await this.checkUser(req, resp, next);
-      const userRepo = UsersMongoRepo.getInstance();
-      const user = await userRepo.search({
-        key: 'email',
-        value: userMail,
-      });
-      req.body.creator = user[0];
-      const result = await this.repo.create(req.body);
+      Customreq.body.creator = Customreq.credentials.id;
+      const result = await this.repo.create(Customreq.body);
       debug('New register created');
       resp.status(201);
       resp.json({
@@ -72,7 +46,7 @@ export class PlantsController {
   async getAll(_req: Request, resp: Response, next: NextFunction) {
     try {
       debug('getAll: get');
-      const result = await this.repo.getAll();
+      const result = await this.repo.findAll;
       resp.status(200);
       return result;
     } catch (error) {
