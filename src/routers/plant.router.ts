cb060721a5dfe74cdd270606a98db5950d7880b4
Daniel <dursuaf@gmail.com>
2023-03-20 19:41:59 +0100

    Update plants router

--- src/routers/plant.router.ts
+++ src/routers/plant.router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { PlantsController } from '../controllers/plant.controller.js';
+import { Interceptors } from '../interceptor/interceptor.js';
 import { PlantsMongoRepo } from '../repositories/plants/plants.mongo.repo.js';
 
 export const plantRouter = Router();
@@ -7,4 +8,4 @@ export const plantRouter = Router();
 const repo = PlantsMongoRepo.getInstance();
 const controller = new PlantsController(repo);
 
-plantRouter.post('/add', controller.add.bind(controller));
+plantRouter.post('/add', Interceptors.logged, controller.add.bind(controller));
