9caa293cfe2e88f28a6328397c42efe0feaf8e73
Daniel <dursuaf@gmail.com>
2023-03-20 12:14:02 +0100

    Update plant controller and test

--- src/controllers/plant.controller.test.ts
+++ src/controllers/plant.controller.test.ts
@@ -1,10 +1,7 @@
 import { HTTPError } from '../errors/error';
 import { PlantsController } from './plant.controller';
-import jwt from 'jsonwebtoken';
 import {
-  mockReq5,
   mockNext,
-  mockReq6,
   mockPlants,
   mockResp,
   mockPlantsComplete,
@@ -14,7 +11,7 @@ import {
 const mockRepo = {
   create: jest.fn(),
   search: jest.fn(),
-  getAll: jest.fn(),
+  findAll: jest.fn(),
 };
 
 const userRepoMock = {
@@ -38,41 +35,6 @@ describe('Given the PlantsController', () => {
       expect(plantsController).toBeInstanceOf(PlantsController);
     });
   });
-  describe('When we use the checkUser method without authorization', () => {
-    test('Then, it should throw an error', () => {
-      plantsController.checkUser(mockReq5, mockResp, mockNext);
-      expect(mockNext).toHaveBeenCalledWith(
-        new HTTPError(400, 'Bad request', 'Request required')
-      );
-    });
-  });
-  describe('When we use the checkUser method with authorization', () => {
-    test('Then, it should call the jwt.verify method', () => {
-      jwt.verify = jest.fn();
-      plantsController.checkUser(mockReq6, mockResp, mockNext);
-      expect(jwt.verify).toHaveBeenCalled();
-    });
-  });
-  describe('When we have a token', () => {
-    test('If it is invalid, then it should throw an error', async () => {
-      jwt.verify = jest.fn();
-      (jwt.verify as jest.Mock).mockResolvedValue('test');
-      await plantsController.checkUser(mockReq6, mockResp, mockNext);
-      expect(mockNext).toHaveBeenCalledWith(
-        new HTTPError(401, 'Unauthorized', 'Invalid token')
-      );
-    });
-    test('If it is valid, then it should return the usermail', async () => {
-      jwt.verify = jest.fn();
-      (jwt.verify as jest.Mock).mockResolvedValue({ email: 'test' });
-      const result = await plantsController.checkUser(
-        mockReq6,
-        mockResp,
-        mockNext
-      );
-      expect(result).toEqual('test');
-    });
-  });
   describe('When we use the add method', () => {
     test('If any of the conditions are missing it should throw an error', () => {
       mockPlants.forEach((plant) => {
@@ -93,10 +55,6 @@ describe('Given the PlantsController', () => {
     test('If the register do  not exist, it should create it', async () => {
       mockRepo.search.mockResolvedValue([]);
       mockRepo.create.mockResolvedValue('test');
-      jest
-        .spyOn(plantsController, 'checkUser')
-        .mockImplementation(async () => 'mocked');
-      userRepoMock.search.mockResolvedValue(['test']);
       await plantsController.add(mockPlantsComplete, mockResp, mockNext);
       expect(mockResp.status).toHaveBeenCalled();
       expect(mockResp.json).toHaveBeenCalled();
@@ -112,7 +70,7 @@ describe('Given the PlantsController', () => {
   });
   describe('When we use the getAll method, if there is no error', () => {
     test('It should return all the data', async () => {
-      mockRepo.getAll.mockResolvedValue(['test']);
+      mockRepo.findAll.mockResolvedValue(['test']);
 
       const element = await plantsController.getAll(
         mockReq,
@@ -124,10 +82,9 @@ describe('Given the PlantsController', () => {
   });
   describe('When we use the getAll method, if there is an error', () => {
     test('It should throw an error', async () => {
-      mockRepo.getAll.mockResolvedValue(null);
+      mockRepo.findAll.mockRejectedValue(new Error());
 
       await plantsController.getAll(mockReq, mockResp, mockNext);
-
       expect(mockNext).toHaveBeenCalled();
     });
   });
--- src/controllers/plant.controller.ts
+++ src/controllers/plant.controller.ts
@@ -4,6 +4,7 @@ import { HTTPError } from '../errors/error.js';
 import jwt from 'jsonwebtoken';
 import { UsersMongoRepo } from '../repositories/users/users.mongo.repo.js';
 import { PlantRepo } from '../repositories/plants/plant.interface.js';
+import { CustomRequest } from '../interceptor/interceptor.js';
 
 const debug = createDebug('WFP:controller: plants');
 
@@ -12,28 +13,28 @@ export class PlantsController {
     debug('Instantiate');
   }
 
-  async add(Customreq: Request, resp: Response, next: NextFunction) {
+  async add(req: CustomRequest, resp: Response, next: NextFunction) {
     try {
       debug('add: post');
       if (
-        !Customreq.body.photo ||
-        !Customreq.body.name ||
-        !Customreq.body.ubication ||
-        !Customreq.body.height ||
-        !Customreq.body.lightness ||
-        !Customreq.body.humidity ||
-        !Customreq.body.difficult ||
-        !Customreq.body.animalFriendly
+        !req.body.photo ||
+        !req.body.name ||
+        !req.body.ubication ||
+        !req.body.height ||
+        !req.body.lightness ||
+        !req.body.humidity ||
+        !req.body.difficult ||
+        !req.body.animalFriendly
       )
         throw new HTTPError(400, 'Bad request', 'Incomplete information');
       const data = await this.repo.search({
         key: 'name',
-        value: Customreq.body.name,
+        value: req.body.name,
       });
       if (data.length)
         throw new HTTPError(409, 'Conflict', 'Register already exist');
-      Customreq.body.creator = Customreq.credentials.id;
-      const result = await this.repo.create(Customreq.body);
+      req.body.creator = req.credentials?.id;
+      const result = await this.repo.create(req.body);
       debug('New register created');
       resp.status(201);
       resp.json({
@@ -46,7 +47,7 @@ export class PlantsController {
   async getAll(_req: Request, resp: Response, next: NextFunction) {
     try {
       debug('getAll: get');
-      const result = await this.repo.findAll;
+      const result = await this.repo.findAll();
       resp.status(200);
       return result;
     } catch (error) {
